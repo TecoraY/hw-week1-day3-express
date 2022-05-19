6d2c54583375d56006bdf86cfdcf4e6d78d203c8
TecoraY <102709566+TecoraY@users.noreply.github.com>
2022-05-18 21:12:59 -0400

    unable to calculate tip, numbers dont work with express

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const app= express();
 const greetings= 'hello how are you';
 
 
-app.get('/greetings/:name', function (req, res) {
-    res.send(greetings+ ' '+ req.params.name);
+
+app.get('/greetings/:name', function (request, response) {
+    response.send(greetings+ ' '+ request.params.name);
+});
+
+app.get("/tip/:total/:tipPercentage", function(request, response){
+	var n1 = request.params.total; 
+	var n2 = request.params.tipPercentage; 
+    var sum= n1 / n2;
+    console.log(sum);
+	response.send(sum + '');
 });
 
 app.listen(8080, function (){
